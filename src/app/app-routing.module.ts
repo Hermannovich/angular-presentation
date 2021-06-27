eae81adaad3a802b452a4a32ac0e1c55948554ee
Hermannovich <donfack.hermann@gmail.com>
2021-06-27 05:52:40 +0200

    - Add Dashboard component - Add Auth component - Setup Route's configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'authentication', component: AuthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ButtonComponent } from './button/button.component';
 import { FlagComponent } from './flag/flag.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { FlagComponent } from './flag/flag.component';
     AppComponent,
     NavBarComponent,
     ButtonComponent,
-    FlagComponent
+    FlagComponent,
+    DashboardComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
