6909eaaf5e74f302d94d2221490e3fd9b070c080
Hermannovich <donfack.hermann@gmail.com>
2021-06-27 05:26:55 +0200

    - Add ng-bootstrap for bootstrap's javascript components - Setup navbar skeleton - Add localize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ButtonComponent } from './button/button.component';
+import { FlagComponent } from './flag/flag.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ButtonComponent,
+    FlagComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
